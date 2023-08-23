a294e9137d56c682de08abe7e427945e44616072
kxh4892636 <kxh4892636@gmail.com>
2023-08-23 16:50:07 +0800

    chore: add swagger

--- package/express/src/route/test.route.ts
+++ package/express/src/route/test.route.ts
@@ -1,28 +1,19 @@
+/* eslint-disable jsdoc/check-tag-names */
 import { Router } from 'express'
 import { testController } from '../controller/test.service.controller'
 import { testMiddleware } from '../middlleware/test.middleware'
 
 export const testRoute = Router()
 
-// /**
-//  * @swagger
-//  * /test:
-//  *   get:
-//  *      description: test
-//  *      tags: [Test]
-//  *      produces:
-//  *          - application/json
-//  *      parameters:
-//  *          - name: test
-//  *            description: test
-//  *            in: json
-//  *            required: true
-//  *            type: string
-//  *      responses:
-//  *          200:
-//  *              description: success
-//  *              schema:
-//  *                  type: string
-//  *                  properties:
-//  */
+/**
+ * @swagger
+ * /test:
+ *   get:
+ *      description: To Check the environment of node.
+ *      tags: [Test]
+ *      responses:
+ *          "200":
+ *              description: success
+ *              type: string
+ */
 testRoute.use(testMiddleware.test, testController.test)
